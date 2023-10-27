8a5ef8097e9340dbd93dda2d202c53d2300e8d37
BFBacchi <bfbacchi@gmail.com>
2023-10-26 21:01:36 -0300

    se corrigen rutas de url para deploy en github pages

--- ticket.js
+++ ticket.js
@@ -1,14 +1,46 @@
+var select = document.getElementById("miSelect");
+
+const valorTicket = 200;
+var valorTotal;
+console.log(cantidad);
+
 function cambiarOpcionSelect(valor) {
-    // Obtén una referencia al elemento select
-    var select = document.getElementById("miSelect");
+  // Cambia la opción seleccionada al valor deseado
+  select.value = valor;
 
-    // Cambia la opción seleccionada al valor deseado
-    select.value = valor;
-
-    // Opcionalmente, puedes disparar un evento "change" en el select
-    var event = new Event('change');
-    select.dispatchEvent(event);
+  // Dispara un evento "change" en el select
+  var event = new Event("change");
+  select.dispatchEvent(event);
 }
 
-   
+function resumen() {
+  event.preventDefault();
+  var cantidadInput = document.getElementById("input-cantidad");
+  var cantidad = cantidadInput.value;
+  var opcion = select.value;
+  var descuento;
+  switch (opcion) {
+    case "1":
+      descuento = 0.2;
 
+      break;
+    case "2":
+      descuento = 0.5;
+
+      break;
+    case "3":
+      descuento = 0.85;
+
+      break;
+    case "4":
+      descuento = 1;
+
+      break;
+  }
+  valorTotal = valorTicket * descuento * cantidad;
+
+  document.getElementById("total").innerHTML = "Total a pagar: $" + valorTotal;
+}
+function borrar() {
+  form.reset();
+}
